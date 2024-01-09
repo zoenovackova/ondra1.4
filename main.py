8da86754aff8f7e7c2fb1088b39dda281ed36259
zoe <novackova.zoe@gmail.com>
2024-01-09 18:34:12 +0100

    1 commit j r s

--- main.py
+++ main.py
@@ -1,77 +1,64 @@
-from turtle import *
-a=input("ahoj napis svoje jmeno")
-print("ahoj "+a)
-d=input("co je tvoje nejoblíbenější barva")
-if d=="modrá":
-    color("blue")
-elif d=="červená":
-    color("red")
-elif d=="zelená":
-    color("green")
-b=input("co chceš nakreslit?")
-if b=="čtverec":
-    from turtle import *
-    shape("turtle")
-    speed(10)
-    forward(100)
-    left(90)
-    forward(100)
-    left(90)
-    forward(100)
-    left(90)
-    forward(100)
-    left(90)
+mistnosti = ["obývák", "chodba", "sklep", "trůnní sál", "jídelna"]
+chodby = [[1, 2], [0], [0], [1, 2, 4], [3]]
+zamcene_chodby = [[], [3], [], [], []]
+klic = 2
 
-elif b=="trojúhelník":
-    from turtle import *
-    shape("turtle")
-    speed(10)
-    forward(100)
-    left(120)
-    forward(100)
-    left(120)
-    forward(100)
-    left(120)
+zlato = [1, 0, 10, 300, 10]
 
-else:
- print("nerozumím, zkus jiný tvar")
- b=input("co chceš nakreslit?")
-if b=="čtverec":
-    from turtle import *
-    shape("turtle")
-    speed(10)
-    forward(100)
-    left(90)
-    forward(100)
-    left(90)
-    forward(100)
-    left(90)
-    forward(100)
-    left(90)
+hrac = 0
+skore = 0
+kroky = 0
+ma_klic = False
 
-elif b=="trojúhelník":
-    from turtle import *
-    shape("turtle")
-    speed(10)
-    forward(100)
-    left(120)
-    forward(100)
-    left(120)
-    forward(100)
-    left(120)
+def hotovo():
+    return sum(zlato) == 0
 
-c=input("máš nějaký oblíbený tvar")
-if c=="ano":
-    input("a jaký je tvůj oblíbený tvar")
-    print("aha")
+while not hotovo():
+    print("hráč je v místnosti:", mistnosti[hrac])
+    print("hráč má", skore, "zlata")
+    print("zbývá zlata:", sum(zlato))
 
-elif c=="ne":
-    print("nevadí")
+    kam_lze_jit = chodby[hrac]
+    for i, moznost in enumerate(kam_lze_jit):
+        print("Moznost", i + 1, ": ", mistnosti[moznost])
 
-else:
-    print("aha")
+    if zlato[hrac] > 0:
+        print("Moznost X : sebrat", zlato[hrac], "zlata")
+
+    if hrac == klic:
+        print("Moznost K : sebrat klíč")
+
+    if ma_klic and zamcene_chodby[hrac]:
+        print("Moznost O : odemknout dveře ->", mistnosti[zamcene_chodby[hrac][-1]])
 
 
+    vstup = input("> ")
+    # vstup_ok = False
+    # while not vstup_ok:
+    #     vstup = input("> ")
+    #
+    #     if vstup == "x" and zlato[hrac] > 0:
+    #         vstup_ok = True
+    #         break
+
+    if vstup == "x":
+        skore += zlato[hrac]
+        zlato[hrac] = 0
+    elif vstup == "k":
+        ma_klic = True
+        klic = -1
+    elif vstup == "o":
+        cilova_mistnost = zamcene_chodby[hrac].pop()
+        chodby[hrac].append(cilova_mistnost)
+        print("Slyšíš jak cvaknul zámek a dveře se otevřely.")
+    else:
+        index_moznosti = int(vstup) - 1
+        hrac = kam_lze_jit[index_moznosti]
+
+    kroky += 1
+    #break
+
+print("Gratuluju, sebral jsi celkem,", skore, "zlata za", kroky, "kroků")
 
 
 
